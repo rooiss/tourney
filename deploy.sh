27f1f502caa3db0d36320a801930ce2ebaf2ca37
rooiss <louis.kim010@gmail.com>
2021-09-20 18:37:00 -0700

    added nginx restart to deploy script;

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ cd ~/tourney
 # pull latest code
 git pull origin master
 
+# restart linux
+sudo /usr/sbin/service nginx restart
+
 # build the docker image for the app
 docker-compose -f ./docker/prod/docker-compose.yml pull
 docker-compose -f ./docker/prod/docker-compose.yml up -d
